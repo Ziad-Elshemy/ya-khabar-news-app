e73f5b2b18351f41a9782b8b7e0171fd7c0cef7f
ziadElshimy <127307908+ziadElshimy@users.noreply.github.com>
2024-06-06 19:20:28 +0300

    color and background shape of item problem solved

--- app/src/main/java/com/example/newsapp/ui/CategoriesAdapter.kt
+++ app/src/main/java/com/example/newsapp/ui/CategoriesAdapter.kt
@@ -5,22 +5,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapp.R
+import com.google.android.material.card.MaterialCardView
 
 class CategoriesAdapter(val categories:List<Category>) : RecyclerView.Adapter<CategoriesAdapter.ViewHolder>(){
 
 
 
-    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-        val title:TextView = itemView.findViewById(R.id.item_title)
-        val image:ImageView=itemView.findViewById(R.id.item_image)
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_category,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            if (viewType == isLeftSided)
+                R.layout.item_category_left_sided
+            else R.layout.item_category_right_sided,
+            parent,false)
         return ViewHolder(view)
     }
+
+    val isRightSided = 10
+    val isLeftSided = 20
+    override fun getItemViewType(position: Int): Int {
+        if (position%2==0) return isLeftSided
+        return isRightSided
+    }
+
     override fun getItemCount(): Int {
         return categories.size
     }
@@ -29,16 +39,28 @@ class CategoriesAdapter(val categories:List<Category>) : RecyclerView.Adapter<Ca
         val cat = categories[position]
         holder.title.setText(cat.titleId)
         holder.image.setImageResource(cat.imageId)
-        holder.itemView.setBackgroundResource(cat.backgroundColor)
+        holder.parent.setCardBackgroundColor(
+            holder.parent.context.getColor(cat.backgroundColor)
+        )
         onItemClickListener.let {
             holder.itemView.setOnClickListener{
                 onItemClickListener?.onItemClick(position,cat)
             }
         }
+
     }
 
     var onItemClickListener :OnItemClickListener? = null
     interface OnItemClickListener{
         fun onItemClick(position: Int,category: Category)
     }
+
+
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val parent:MaterialCardView = itemView.findViewById(R.id.parent)
+        val child:ConstraintLayout = itemView.findViewById(R.id.child)
+        val title:TextView = itemView.findViewById(R.id.item_title)
+        val image:ImageView=itemView.findViewById(R.id.item_image)
+    }
+
 }
